204342fde4d9beb05eb58a1b5d992b4265a41ee5
Irene-Busah <i.busah@alumni.alueducation.com>
2024-08-08 15:06:20 +0200

    completed update profile script with slight changes to the find user function

--- src/core/infra/scripts/find_user.sh
+++ src/core/infra/scripts/find_user.sh
@@ -1,24 +1,34 @@
 #! /bin/bash
 
-USER_STORE="$PWD/core/infra/data/user-store.txt"
+USER_STORE="$HOME/PrognosisConsoleApp/src/core/infra/data/user-store.txt" # Change the file path
+# "$PWD/core/infra/data/user-store.txt"
 
 function find_user() {
     local uuid="$1"
 
-echo "Looking for user" + $uuid
+    # echo "Looking for user: $uuid"
+    found_user=false
     while IFS=',' read -r uUID stored_email role isProfileComplete firstName lastName stored_password userId dateOfBirth isHIVPositive diagnosisDate isOnART ARTStartDate countryISO; do
+        
         if [[ "$uuid" == "$uUID" ]]; then
-            echo "$firstName, $lastName, $uuid"
-            exit 0
-        else
-            echo "Could not find user."
-            exit 1
+            # returning the user object based on the role to ease the update
+            if [[ "$role" == "ADMIN" ]]; then
+                echo "$uUID,$stored_email,$role,$isProfileComplete,$firstName,$lastName,$stored_password,$userId"
+            else
+                echo "$uUID,$stored_email,$role,$isProfileComplete,$firstName,$lastName,$stored_password,$userId,$dateOfBirth,$isHIVPositive,$diagnosisDate,$isOnART,$ARTStartDate,$countryISO"
+            fi
+            found_user=true
+            break
+            return 0
         fi
-
     done < "$USER_STORE"
 
-    echo "Something went wrong while trying to find user."
-    exit 1
+    if [ "$found_user" = true ]; then
+        return 0
+    else
+        echo "Could not find user."
+        return 1
+    fi
 }
 
 find_user "$1"
